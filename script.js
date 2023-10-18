d7df7bb785e9e590152e39fb2455035b90ad74b3
Bora Ozaras <ahmetbozaras@gmail.com>
2023-10-18 23:45:38 +0300

    google maps için konum düzenlemesi

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+function initMap() {
+  var location = { lat: 40.7128, lng: -74.0060 }; // Konum koordinatlarını burada ayarlayın
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: location,
+      zoom: 15
+  });
+
+  var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      title: 'Konumunuz'
+  });
+}
